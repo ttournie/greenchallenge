08125abde27c1a418eee3ed608d4485c82b56c47
tto <tto@Terre.localdomain>
2013-02-14 17:12:19 +0100

    changing form redirection

--- sites/all/themes/green_challenge/template.php
+++ sites/all/themes/green_challenge/template.php
@@ -5,9 +5,5 @@
  * Override or insert variables into the page template.
  */
 function green_challenge_process_page(&$variables) {
-  //Hide the page title
-  dsm($variables);
-    
-   //$variables['title'] = NULL;
  
 }
