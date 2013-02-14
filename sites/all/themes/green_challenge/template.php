815e4c1ee4000f11ce828d4b229e262009480ec0
tto <tto@Terre.localdomain>
2013-02-14 16:04:26 +0100

    hiding node title

--- sites/all/themes/green_challenge/template.php
+++ sites/all/themes/green_challenge/template.php
@@ -1,5 +1,13 @@
 <?php
+
+
 /**
- * @file
- * template.php
+ * Override or insert variables into the page template.
  */
+function green_challenge_process_page(&$variables) {
+  //Hide the page title
+  dsm($variables);
+    
+   //$variables['title'] = NULL;
+ 
+}
