7746c53cec550db8db1e3b1233387460c156319c
tto <tto@Terre.localdomain>
2013-02-15 16:10:08 +0100

    hiding profile title

--- sites/all/themes/green_challenge/template.php
+++ sites/all/themes/green_challenge/template.php
@@ -1,9 +1,22 @@
 <?php
 
-
 /**
- * Override or insert variables into the page template.
+ * Process variables for user-profile-category.tpl.php.
+ *
+ * The $variables array contains the following arguments:
+ * - $element
+ *
+ * @see user-profile-category.tpl.php
  */
-function green_challenge_process_page(&$variables) {
- 
+function green_challenge_preprocess_user_profile_category(&$variables) {
+  // Hide title in certain case
+  if($variables['element']['#title'] == 'Main profile') {
+     $variables['title'] = null;  
+  } else 
+    $variables['title'] = check_plain($variables['element']['#title']);
+  $variables['profile_items'] = $variables['element']['#children'];
+  $variables['attributes'] = '';
+  if (isset($variables['element']['#attributes'])) {
+    $variables['attributes'] = drupal_attributes($variables['element']['#attributes']);
+  }
 }
